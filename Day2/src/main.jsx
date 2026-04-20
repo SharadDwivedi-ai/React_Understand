396997ae12c62b3f0ad54535e1de9c2200664dbf
SharadDwivedi-ai <srd28nov21@gmail.com>
2026-04-21 01:40:54 +0530

    some custom react update and check

--- Day2/src/main.jsx
+++ Day2/src/main.jsx
@@ -1,9 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+
+// Custom react manipulation
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit google",
+// }
+// const anotherElement = (
+//   <a href="http://google.com" target='_blank'>Google</a>
+// )
+
+// const reactElement = React.createElement(
+//   'h1',
+//   { type: 'text' },
+//   'Welcome to custom manipulate'
+// )
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />
+  // anotherElement
+  // reactElement
 )
