422b5e46501cf7641e8002a6df86caf61178796c
SharadDwivedi-ai <srd28nov21@gmail.com>
2026-04-21 01:47:37 +0530

    initial commit

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -1,12 +1,12 @@
 function customRender(reactElement, container) {
-  // Previous version
+  // Basic approach
   // const domElement = document.createElement(reactElement.type);
   // domElement.innerHTML = reactElement.children;
   // domElement.setAttribute("href", reactElement.props.href);
   // domElement.setAttribute("target", reactElement.props.target);
   // container.appendChild(domElement);
 
-  // Modular version
+  // Modular approach
   const domElement = document.createElement(reactElement.type);
   domElement.innerHTML = reactElement.children;
   for (const prop in reactElement.props) {
